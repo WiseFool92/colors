7ac2b1fafa4d000d557f72a067f22c609294967e
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-03-24 13:55:46 -0700

    js

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").addClass("greem-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").addClass("red-background");
+  });
+});
+  
